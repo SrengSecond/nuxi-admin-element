e811e59eb622a0d2b1825a6574233eb84d80bb60
SrengSecond <stickercenter2@gmail.com>
2022-10-24 16:18:55 +0700

    Init

---
+++ components/navbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul>
+    <li v-for="({title,path},index) in links">
+      <NuxtLink :to="path" :key="index">{{ title }}</NuxtLink>
+    </li>
+  </ul>
+</template>
+
+<script setup>
+const links = [
+  {
+    title: "Home",
+    path: "/"
+  },
+  {
+    title: "About",
+    path: "/about"
+  },
+  {
+    title: "Payment",
+    path: "/payment"
+  },
+  {
+    title: "Contact",
+    path: "/contact"
+  },
+]
+</script>
